9ee81da017c5c00f30b1dfa53b18d76c6a556a48
MingoDynasty <github@mingodynasty.com>
2022-06-27 00:33:42 -0700

    Add Flyway to automatically update the database.

---
+++ src/main/resources/db/migrations/V00001__init.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA IF NOT EXISTS marvel;
+GRANT ALL ON SCHEMA marvel TO PUBLIC;
+
+CREATE TABLE IF NOT EXISTS characters
+(
+    id             INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    marvel_id      INT                                    NOT NULL UNIQUE,
+    name           VARCHAR,
+    description    VARCHAR,
+    thumbnail_path VARCHAR,
+    last_retrieved TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
+);
