2dae6e8e7662dab588a0c032c426b8e49723e079
MingoDynasty <github@mingodynasty.com>
2022-06-26 21:58:07 -0700

    Add getCharacter endpoint.

--- src/main/java/com/example/springboot/MarvelController.java
+++ src/main/java/com/example/springboot/MarvelController.java
@@ -1,25 +1,34 @@
 package com.example.springboot;
 
+import com.arnaudpiroelle.marvel.api.objects.Character;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-public class HelloController {
-    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+@RequestMapping("/")
+public class MarvelController {
+    private static final Logger logger = LoggerFactory.getLogger(MarvelController.class);
 
-    @GetMapping("/")
-    public String index() {
-        logger.debug("mingtest debug");
-        logger.warn("mingtest warn");
-        logger.info("mingtest info");
-        logger.error("mingtest error");
+    @Autowired
+    private MarvelService marvelService;
 
-        System.out.println(System.getProperty("java.version"));
-        System.out.println(System.getProperty("java.specification.version"));
+    @GetMapping("/characters")
+    public ResponseEntity<Object> getCharacter(@RequestParam String name) {
+        logger.debug("mingtest get start");
 
-        return "Greetings from Spring Boot!";
+        List<Character> characters = marvelService.listCharacter(name);
+
+        logger.debug("mingtest get done");
+        return new ResponseEntity<>(characters, HttpStatus.OK);
     }
 
 }
